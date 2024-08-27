4eb61728799909e77cfa4ec1287c4c181175f166
MaddyMk05 <maadhava05@gmail.com>
2024-08-27 10:36:00 +0530

    ABCmax.java

---
+++ ABCmax.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String[] args){
+   
+        int a=10,b=40,c=30;
+        System.out.print((a>b)?((a>c)?"a is max":"c is max"):((b>c)?"b is max":"c is max"));
+    }
+}
