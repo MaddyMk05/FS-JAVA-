a583e7a4b58dbc84b8d40546c0d6ffb0ff279856
MaddyMk05 <maadhava05@gmail.com>
2024-08-28 15:27:51 +0530

    ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner aakash=new Scanner(System.in);
+        String s=aakash.next();
+        for (int i=0;i<s.length();i++)
+        {
+        int maddy=s.charAt(i);
+        System.out.println(maddy);
+        }
+    }
+}
